adc50b81085da369e7df40ab8e947b80756864c3
Wieland <29458682+wielandb@users.noreply.github.com>
2024-06-11 15:32:34 +0200

    Refactor Maproulette Challenge Factory

---
+++ challenges/large-stop_area-bbox/main.py
@@ -0,0 +1,67 @@
+import sys, math
+sys.path.append('../../include')
+import MRFactory as mrf
+
+## Functions specific to this challenge
+
+def get_distance_from_coordinates(lat1, lon1, lat2, lon2):
+    R = 6371000  # Radius of the Earth in meters
+    lat1 = math.radians(lat1)
+    lon1 = math.radians(lon1)
+    lat2 = math.radians(lat2)
+    lon2 = math.radians(lon2)
+    dlat = lat2 - lat1
+    dlon = lon2 - lon1
+    a = math.sin(dlat / 2) * math.sin(dlat / 2) + math.cos(lat1) * math.cos(lat2) * math.sin(dlon / 2) * math.sin(dlon / 2)
+    c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
+    return R * c
+
+# Use this function to determine if a task needs to be created for a given element
+# use this function for filtering things that overpass cannot filter, maybe by using a function from a different file that you specifically implemented for this task
+# if your overpass query already returns all elements that need to be fixed, make this function return True
+def needsTask(e):
+    # Every element looks like this:
+    # {"type": "relation", "id": 4751, "bounds": {"minlat": 51.4922560, "minlon": 7.4171198, "maxlat": 51.4923844, "maxlon": 7.4176189 }},
+    # An element needs a task if either the vertical or the horizontal bbox edge is longer than a predefinded value in meters
+    max_distance = 300
+    # Calculate the length of the longitude difference of the bbox
+    lon_diff = get_distance_from_coordinates(e['bounds']['minlat'], e['bounds']['minlon'], e['bounds']['minlat'], e['bounds']['maxlon'])
+    # Calculate the length of the latitude difference of the bbox
+    lat_diff = get_distance_from_coordinates(e['bounds']['minlat'], e['bounds']['minlon'], e['bounds']['maxlat'], e['bounds']['minlon'])
+    # If either the longitude or the latitude difference is longer than 100 meters, return the name of the element
+    print(lon_diff, lat_diff)
+    if lon_diff > max_distance or lat_diff > max_distance:
+        return True
+
+
+
+opQuery = """
+[out:json][timeout:250];
+area(id:3600051477)->.searchArea;
+relation["public_transport"="stop_area"]["operator"!~"DB"]["train"!="yes"]["railway"!="facility"](area.searchArea);
+out bb ids;
+"""
+
+op = mrf.Overpass()
+resultElements = op.getElementsFromQuery(opQuery)
+
+challenge = mrf.Challenge()
+
+for element in resultElements:
+	if needsTask(element):
+		bboxGeometry = mrf.getElementGeometry(element)
+		print(bboxGeometry)
+		mainFeature = mrf.MainGeoFeature(
+			geometry=bboxGeometry, 
+			properties={}, 
+			osmType="relation", 
+			osmId=element["id"])
+		t = mrf.Task(
+			mainFeature=mainFeature, 
+			additionalFeatures=[], 
+			cooperativeWork=None)
+		challenge.addTask(t)
+
+challenge.saveToFile("large_stop_area_bbox.json")
+
+	
--- challenges/stop_area-names-from-platform-names/main.py
+++ challenges/stop_area-names-from-platform-names/main.py
@@ -1,59 +1,8 @@
-import json
-import urllib.parse
-import requests
-import re
-# Imports that are specific to this challenge
+import sys
+sys.path.append('../../include')
+import MRFactory as mrf
 
-
-
-# Use this function to determine if a task needs to be created for a given element
-# use this function for filtering things that overpass cannot filter
-def needsTask(e):
-    return True
-
-## Helper functions
-
-# For some currently unknown reason, sometimes special characters are mangled in the data
-# we pass every string through that function to correct the problem if it appears
-def correct_mangled_characters(input_string):
-    mangled_to_correct = {
-        'Ã¼': 'ü',
-        'Ã¶': 'ö',
-        'Ã¤': 'ä',
-        'ÃŸ': 'ß',
-        'Ã ': 'à',
-        'Ã¡': 'á',
-        'Ã¢': 'â',
-        'Ã£': 'ã',
-        'Ã©': 'é',
-        'Ã¨': 'è',
-        'Ãª': 'ê',
-        'Ã«': 'ë',
-        'Ã®': 'î',
-        'Ã¯': 'ï',
-        'Ã´': 'ô',
-        'Ãµ': 'õ',
-        'Ã¹': 'ù',
-        'Ãº': 'ú',
-        'Ã»': 'û',
-        'Ã½': 'ý',
-        'Ã¿': 'ÿ',
-        'Ã–': 'Ö',
-        'Ãœ': 'Ü',
-        'Ã„': 'Ä',
-        '채': 'ä',
-        'Ã¶': 'ö'
-    }
-    corrected_string = input_string
-    for mangled, correct in mangled_to_correct.items():
-        corrected_string = re.sub(mangled, correct, corrected_string)
-    return corrected_string
-
-
-
-TASKS = []
-
-overpass_query = """
+opQuery = """
 [out:json][timeout:250];
 area(id:3600051477)->.searchArea;
 rel["public_transport"="stop_area"][!"name"](area.searchArea)->.stop_areas;
@@ -76,74 +25,29 @@ foreach.stop_areas -> .stop_area {
       out center tags;
   }
 }
-
 """
 
-# URL encode the query and get the Data from the overpass API
-overpass_url = "http://overpass-api.de/api/interpreter"
-response = requests.get(overpass_url, params={'data': overpass_query})
-data = response.json()
+op = mrf.Overpass()
+resultElements = op.getElementsFromQuery(opQuery)
 
-# Iterate over the data
-for element in data['elements']:
-    # Do anything that needs to be done to the data in a algorithmic way here
-    if value := needsTask(element):
-        # Get lon and lat
-        # if the element is a node, it has a 'lat' and 'lon' key
-        # if the element is a way or a relation, it has a 'center' key with a 'lat' and 'lon' key
-        print(element)
-        if 'lat' in element:
-            element['lat'] = element['lat']
-            element['lon'] = element['lon']
-        else:
-            element['lat'] = element['geometry']['coordinates'][1]
-            element['lon'] = element['geometry']['coordinates'][0]
-        # Create a Task for the element
-        task = {
-            "type": "FeatureCollection",
-            "features": [
-                {
-                    "type": "Feature",
-                    "geometry": {
-                        "type": "Point",
-                        "coordinates": [element['lon'], element['lat']]
-                    },
-                    "properties": {
-                        "@id": f"relation/{element['id']}",
-                        "latitude": element['lat'],
-                        "longitude": element['lon'],
-                        #"name": element.get('tags', {}).get('name', 'Unnamed')
-                    }
-                }
-            ],
-            "cooperativeWork": {
-                "meta": {
-                    "version": 2,
-                    "type": 1
-                },
-                "operations": [
-                    {
-                        "operationType": "modifyElement",
-                        "data": {
-                            "id": f"relation/{element['id']}",
-                            "operations": [
-                                {
-                                    "operation": "setTags",
-                                    "data": {
-                                        "name": element['tags']['name']
-                                    }
-                                }
-                            ]
-                        }
-                    }
-                ]
-            }
-        }
-        TASKS.append(task)
+challenge = mrf.Challenge()
 
-with open('tasks.json', 'w', encoding="UTF-8") as f:
-    for task in TASKS:
-        f.write('\x1E')
-        json.dump(task, f, ensure_ascii=False)
-        f.write('\n')
+for element in resultElements:
+	centerPoint = mrf.getElementCenterPoint(element)
+	mainFeature = mrf.MainGeoFeature(
+		geometry=centerPoint, 
+		properties={}, 
+		osmType="relation", 
+		osmId=element["id"])
+	suggestedName = element["tags"]["name"]
+	cooperativeWork = mrf.TagFix(
+		osmType="relation", 
+		osmId=element["id"], 
+		tags={"name": suggestedName})
+	t = mrf.Task(
+		mainFeature=mainFeature, 
+		additionalFeatures=[], 
+		cooperativeWork=cooperativeWork)
+	challenge.addTask(t)
 
+challenge.saveToFile("stop_area_names_from_platform_names.json")
