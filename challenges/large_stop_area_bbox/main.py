497547d846ae38832716e0c23b5f618fa848d239
Wieland <29458682+wielandb@users.noreply.github.com>
2024-06-25 14:25:03 +0200

    Fix attempt 3

--- challenges/large_stop_area_bbox/main.py
+++ challenges/large_stop_area_bbox/main.py
@@ -1,8 +1,8 @@
 import sys, math
-#sys.path.append('../../shared')
+sys.path.append('shared')
 #from ..shared import challenge_builder as mrcb
 #import challenge_builder as mrcb
-import shared.challenge_builder as mrcb
+import challenge_builder as mrcb
 from geopy import distance
 
 ## Functions specific to this challenge
