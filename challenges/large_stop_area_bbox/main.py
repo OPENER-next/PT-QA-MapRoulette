10b7a608790483558cb8afc179fbdf7ebf3a9af5
Wieland <29458682+wielandb@users.noreply.github.com>
2024-06-26 14:40:47 +0200

    Make the path static from everywhere

--- challenges/large_stop_area_bbox/main.py
+++ challenges/large_stop_area_bbox/main.py
@@ -1,5 +1,6 @@
 import sys, math
-sys.path.append('shared')
+from pathlib import Path
+sys.path.append(str(Path(__file__).parent.parent.parent / "shared"))
 #from ..shared import challenge_builder as mrcb
 #import challenge_builder as mrcb
 import challenge_builder as mrcb
